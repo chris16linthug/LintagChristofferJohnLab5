d4ae99b73e4b59e3684888c517b2c4e579dda5d4
chris16linthug <2015081175@ust-ics.mygbiz.com>
2018-10-08 20:28:06 +0800

    Initial commit

---
+++ app/src/main/java/com/lintag/christofferjohn/Activity2.java
@@ -0,0 +1,28 @@
+package com.lintag.christofferjohn;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Activity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void process(View v){
+        Intent i=null, chooser=null;
+        if(v.getId()==R.id.btnScreen1){
+            i = new Intent(this,MainActivity.class);
+            startActivity(i);
+        } else if(v.getId()==R.id.btnMap2){
+            i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo:14.5321,120.9838"));
+            chooser = Intent.createChooser(i, "Open with?");
+            startActivity(chooser);
+        }
+    }
+}
---
+++ app/src/main/java/com/lintag/christofferjohn/MainActivity.java
@@ -0,0 +1,32 @@
+package com.lintag.christofferjohn;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(this, Service.class);
+        startActivity(i);
+    }
+
+    public void process(View v){
+        Intent i=null, chooser=null;
+        if(v.getId()==R.id.btnScreen2){
+            i = new Intent(this,Activity2.class);
+            startActivity(i);
+        } else if(v.getId()==R.id.btnMap){
+            i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse("geo:14.6207,121.0534"));
+            chooser = Intent.createChooser(i, "Open with?");
+            startActivity(chooser);
+        }
+    }
+}
